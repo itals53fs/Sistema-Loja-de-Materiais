fda2aff827790a522632bc63693258640ad9ee24
Tales Félix <tales.felix.10@gmail.com>
2020-12-02 20:12:21 -0200

    first commit

---
+++ tp02/src/Material.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tales
+ */
+public class Material {
+    private String nome;
+    private int quantidade;
+    private float preco;
+    private String especificacao;
+    private float margemLucro;
+    // colocar date
+    private String fornecedor;
+}
